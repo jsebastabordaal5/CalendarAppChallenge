05aff23ebdba41706e527079d9e3a7005a866f83
Juanse <jtaborda579@soyudemedellin.edu.co>
2024-09-11 13:17:02 -0500

    modify class Calendar

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from datetime import datetime, date, time
+from datetime import datetime, date, time, timedelta
 from typing import ClassVar
 
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
@@ -56,12 +56,61 @@ class Day:
     def __init__(self, date_: date):
         self.date_: date = date_
         self.slots: dict[time, str | None] = {}
+        self._init_slots()
 
+    def _init_slots(self):
+        current_time = time(0, 0)
+        end_time = time(23, 45)
+        slot_duration = timedelta(minutes=15)
 
+        while current_time <= end_time:
+            self.slots[current_time] = None
+            current_datetime = datetime.combine(self.date_, current_time) + slot_duration
+            current_time = current_datetime.time()
 
+    def add_event(self, event_id: str, start_at: time, end_at: time):
+        current_time = start_at
 
+        while current_time < end_at:
+            if self.slots.get(current_time) is not None:
+                slot_not_available_error()
+                return
 
+            current_datetime = datetime.combine(self.date_, current_time) + timedelta(minutes=15)
+            current_time = current_datetime.time()
+
+        current_time = start_at
+        while current_time < end_at:
+            self.slots[current_time] = event_id
+            current_datetime = datetime.combine(self.date_, current_time) + timedelta(minutes=15)
+            current_time = current_datetime.time()
+
+    def delete_event(self, event_id: str):
+        deleted = False
+        for slot, saved_id in self.slots.items():
+            if saved_id == event_id:
+                self.slots[slot] = None
+                deleted = True
+        if not deleted:
+            event_not_found_error()
+
+    def update_event(self, event_id: str, start_at: time, end_at: time):
+        for slot in self.slots:
+            if self.slots[slot] == event_id:
+                self.slots[slot] = None
+
+        for slot in self.slots:
+            if start_at <= slot < end_at:
+                if self.slots[slot]:
+                    slot_not_available_error()
+                else:
+                    self.slots[slot] = event_id
 
 
 class Calendar:
-    pass
+
+    def __init__(self):
+        self.days: dict[date, Day] = {}
+        self.events: dict[str, Event] = {}
+
+    def add_event(self, title: str, ):
